4eccadf8df2061bd360dffbbcbe81972f439a275
Jilin Men <1032764332@qq.com>
2025-02-12 16:14:35 -0600

    agh.py
    modify return value

--- acolite/gee/agh.py
+++ acolite/gee/agh.py
@@ -1219,5 +1219,4 @@ def agh(image, imColl, rsrd = {}, lutd = {}, luti = {}, settings = {}):
                     print('Removing {}'.format(zfile))
                     os.remove(zfile)
 
-    return({gatts['acolite_type']:ofile,
-           'rhos':i_rhos})
+    return i_rhos
